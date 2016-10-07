89b861bba5ca9fe6913095611f86b87de15a046c
김성찬 <dlrj1234@naver.com>
2016-10-07 17:49:11 +0900

    B04.java 실행완료

--- B04.java
+++ B04.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class B04 {
-       public static void main(Sgirng[] args){
+       public static void main(String[] args){
           Scanner s = new Scanner(System.in);       
           System.out.print("===================");
           System.out.print("연봉을 입력하십시오");
-          money = s.nextInt();
+          double money = s.nextInt();
          if(money >= 80000000){
-           money = money/37 
+           money = money*0.37; 
          }
-         if(money >= 40000000 && money < 80000000){
-           money = money/28 
+         else if(money >= 40000000 && money < 80000000){
+           money = money*0.28; 
          }
-         if(money >= 10000000 && money < 40000000){
-           money = money/19 
+         else if(money >= 10000000 && money < 40000000){
+           money = money*0.19; 
          }
-         if(money < 10000000){
-           money = money/9.5 
+         else if(money < 10000000){
+           money = money*0.095; 
          }
          System.out.print("===================");  
          System.out.print("연봉 금액에 대한 소득세는" +money+ "입니다");
